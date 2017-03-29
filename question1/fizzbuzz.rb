b64ea42b7f92dc7dc30e21fc7bec636b294fe484
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-29 13:36:42 +0900

    q1 リファクタリング

--- question1/fizzbuzz.rb
+++ question1/fizzbuzz.rb
@@ -3,7 +3,7 @@ puts '数字を入力してください'
 count = gets.to_i
 
 output = (1...count).each_with_object([]) do |num, arr|
-  arr << if (num % 3).zero? && (num % 5).zero?
+  arr << if (num % 15).zero?
            'Fizz Buzz'
          elsif (num % 3).zero?
            'Fizz'
