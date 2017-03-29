ed07518b022ceac691343e97ac032099aa8e6397
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-29 14:31:16 +0900

    q5 空のメソッド6種類と配列を用意

---
+++ question5/algorithm.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+def sum(nums)
+
+end
+
+def avg(nums)
+
+end
+
+def max(nums)
+
+end
+
+def min(nums)
+
+end
+
+def sort_asc(nums)
+
+end
+
+def sort_desc(nums)
+
+end
+
+nums = [20, 31, 42, 13, 5, 38]
