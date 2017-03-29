9a34b1dac379ab859776649e4b640256731766f5
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-29 11:13:33 +0900

    q2 hash.fetch()を使ってリファクタリング

--- question2/string_hash.rb
+++ question2/string_hash.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 words = gets.chomp.split
 words_count = words.each_with_object({}) do |word, hash|
-  hash[word] = hash[word].nil? ? 1 : hash[word] + 1
+  hash[word] = hash.fetch(word, 0) + 1
 end
 puts words_count
