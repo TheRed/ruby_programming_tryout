2ad982b7a9a2551d4b8b2e5f69ce362cae4381b1
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-29 14:49:46 +0900

    q5 max() min()実装

--- question5/algorithm.rb
+++ question5/algorithm.rb
@@ -10,11 +10,15 @@ def avg(nums)
 end
 
 def max(nums)
-
+  nums.each.reduce do |num, max|
+    num > max ? num : max
+  end
 end
 
 def min(nums)
-
+  nums.each.reduce do |num, min|
+    num < min ? num : min
+  end
 end
 
 def sort_asc(nums)
@@ -28,3 +32,5 @@ end
 p nums = [20, 31, 42, 13, 5, 38]
 puts "合計: #{sum(nums)}"
 puts "平均: #{avg(nums)}"
+puts "最大値: #{max(nums)}"
+puts "最小値: #{min(nums)}"
