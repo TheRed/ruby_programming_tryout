225649ea80f6c86587a4246f772ad9b6fc38cb1e
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-29 10:43:20 +0900

    q1 Rubocop Auto-collect適用

--- question1/fizzbuzz.rb
+++ question1/fizzbuzz.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
 puts '数字を入力してください'
 count = gets.to_i
 
 output = (1...count).each_with_object([]) do |num, arr|
-  if (num % 3).zero? && (num % 5).zero?
-    arr << 'Fizz Buzz'
-  elsif (num % 3).zero?
-    arr << 'Fizz'
-  elsif (num % 5).zero?
-    arr << 'Buzz'
-  else
-    arr << num
-  end
+  arr << if (num % 3).zero? && (num % 5).zero?
+           'Fizz Buzz'
+         elsif (num % 3).zero?
+           'Fizz'
+         elsif (num % 5).zero?
+           'Buzz'
+         else
+           num
+         end
 end
 puts output.join(', ')
