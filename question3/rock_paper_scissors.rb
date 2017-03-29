f92bceac6d3dcfa0112c33048661d8fe53206539
Hisaki Akaza <TheRed@users.noreply.github.com>
2017-03-29 17:45:50 +0900

    q3review メソッドに分けすぎた処理を統合

--- question3/rock_paper_scissors.rb
+++ question3/rock_paper_scissors.rb
@@ -2,17 +2,8 @@
 class RockPaperScissors
   HANDS = ['グー', 'チョキ', 'パー']
 
-  def set_user_hand
-    input_num = gets.to_i
-    @user_hand = input_num
-  end
-
-  def set_comp_hand
-    @comp_hand = rand(0..HANDS.length)
-  end
-
-  def winner
-    case [@user_hand, @comp_hand]
+  def winner(user_hand, comp_hand)
+    case [user_hand, comp_hand]
     when [0, 1], [1, 2], [2, 0] then
       'あなた'
     when [1, 0], [2, 1], [0, 2] then
@@ -20,25 +11,29 @@ class RockPaperScissors
     end
   end
 
+  def hands_to_s
+    str = '> '.dup
+    HANDS.each_with_index { |hand, i| str << "#{i}.#{hand} " }
+    str
+  end
+
   def run
     loop do
       puts '「じゃんけん・・・」'
-      print '> '
-      HANDS.each_with_index do |hand, i|
-        print "#{i}.#{hand} "
-      end
-      puts
-      set_user_hand || (puts '0, 1, 2 のいずれかを入力してください。'; next)
-      set_comp_hand
+      puts hands_to_s
+      user_hand = gets.to_i
+      comp_hand = rand(0...HANDS.length)
       puts <<~EOF
           「ぽい！」
-          ＊コンピュータ：#{HANDS[@comp_hand]}
-          ＊あなた：#{HANDS[@user_hand]}
+          ＊コンピュータ：#{HANDS[comp_hand]}
+          ＊あなた：#{HANDS[user_hand]}
           EOF
-      break if winner
+      if user_hand != comp_hand
+        puts "「#{winner(user_hand, comp_hand)}の勝ち！」"
+        break
+      end
       puts '「アイコでしょ！」'
     end
-    puts "「#{winner}の勝ち！」"
   end
 end
 
